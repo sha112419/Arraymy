cb39c69b6545b1dcec8c4e67a0f690d7cc9ff4d0
sha112419 <104012644+sha112419@users.noreply.github.com>
2022-05-12 14:18:31 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,21 @@
+package testing;
+
+public class array2 {
+
+	public static void main(String[] args) {
+		int[]task= {11,24,19,2,11,24};
+		System.out.println(task[1]);
+		System.out.println(task[2]);
+		System.out.println(task[4]);
+		System.out.println(task[5]);
+		System.out.println(task[0]);
+
+	 String[]task1= {"sha","ra","rah","far","mech"};
+	   System.out.println(task1[0]);
+	   System.out.println(task1[1]);
+	   System.out.println(task1[2]);
+	   System.out.println(task1[3]);
+	   System.out.println(task1[4]);
+	}
+
+}
